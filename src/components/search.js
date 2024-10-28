d8033a4611ceed8aeb6161a39ba415f2a8ed1f34
Sahith-Rao <sahithraolingampally@gmail.com>
2024-10-28 21:19:27 +0530

    popup windom is also added

--- src/components/search.js
+++ src/components/search.js
@@ -1,15 +1,17 @@
-function Search({handleInput , search}) {
-    return (
-        <section className="searchbox-wrap">
-            <input 
-            type="text" 
-            placeholder="Search for a movie" 
-            className="searchbox" 
-            onChange={handleInput}
-            onKeyPress={search}
-            />
-        </section>
-    )
+import React from 'react'
+
+function Search ({ handleInput, search }) {
+	return (
+		<section className="searchbox-wrap">
+			<input 
+				type="text" 
+				placeholder="Search for a movie..." 
+				className="searchbox" 
+				onChange={handleInput}
+				onKeyPress={search}
+			/>
+		</section>
+	)
 }
 
 export default Search
